0013e8fc5e67f7ffdeb775a0ae0b7133c24c3d61
AbuSohan <sohan@inflexionpointbd.com>
2022-02-07 17:29:45 +0600

    hint added for hangman game

--- hangman.py
+++ hangman.py
@@ -12,6 +12,8 @@ def get_valid_word(words):
 
 def hangman():
     word = get_valid_word(words)
+    hint = [letter for letter in word[:1]] + ['-' for letter in word[2:]] + [letter for letter in word[-1:]]
+    print('Hint: ', ''.join(hint))
     word_letters = set(word)    # letters in the word
     alphabet = set(string.ascii_uppercase)      # get all letters in uppercase
     used_letters = set()    # what the user has guessed
@@ -22,7 +24,7 @@ def hangman():
     while len(word_letters) > 0 and lives > 0:
         # letters used
         # ''.join(['a', 'b', 'cd']) --> 'a b c cd'
-        print('You have', lives, 'lives left and You have used these letters: ', ''.join(used_letters))
+        print('\nYou have', lives, 'lives left and You have used these letters: ', ''.join(used_letters))
 
         # what current word is (ie W - R D)
         word_list = [letter if letter in used_letters else '-' for letter in word]
@@ -33,7 +35,6 @@ def hangman():
             used_letters.add(user_letter)   # add a letter in used_letter list
             if user_letter in word_letters:
                 word_letters.remove(user_letter)    # remove the matching letter from word_letter
-
             else:
                 lives = lives - 1   # takes away a life if wrong
                 print('Letter is not in the word.')
